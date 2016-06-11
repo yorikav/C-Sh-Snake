b41b72429f3cf37d8b6787d7f54dd445ff48b87d
yorikav <asonovyury@gmail.com>
2016-06-11 18:38:18 +0500

    Адаптировали программу под применение нового класса

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            Draw(p1.x, p1.y, p1.sym);
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            Draw(p2.x, p2.y, p2.sym);
+
+            /*int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
 
@@ -20,7 +32,7 @@ namespace Snake
             int y2 = 5;
             char sym2 = '#';
 
-            Draw(x2, y2, sym2);
+            Draw(x2, y2, sym2);*/
 
             Console.ReadLine();
         }
