888d2210b165587d33dc9ea3b95a203c6002a756
yorikav <asonovyury@gmail.com>
2016-06-11 18:21:47 +0500

    Модернизировали программу, добавили функцию Draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,17 +14,20 @@ namespace Snake
             int y1 = 3;
             char sym1 = '*';
 
-            Console.SetCursorPosition(x1, y1);
-            Console.WriteLine(sym1);
+            Draw(x1, y1, sym1);
 
             int x2 = 3;
             int y2 = 5;
             char sym2 = '#';
 
-            Console.SetCursorPosition(x2, y2);
-            Console.WriteLine(sym2);
+            Draw(x2, y2, sym2);
 
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
         }
     }
 }
