fc82673ba9f7ed5cd9f5002894c5ed4dbe3daf87
yorikav <asonovyury@gmail.com>
2016-06-11 23:38:56 +0500

    Знакомимся со списками

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,10 +11,42 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3,  '*');
-            p1.Draw();
+            //p1.Draw();
             
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //p2.Draw();
+
+            Point p3 = new Point(2, 7, '$');
+
+            Point p4 = new Point(7, 8, '&');
+
+            Console.SetCursorPosition(0,p2.y+1);
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            for (int i = 0; i < pList.Count ; i++)
+            {
+                Point p = pList[i];
+                p.Draw();
+            }
 
             Console.ReadLine();
         }
