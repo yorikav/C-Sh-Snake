3467fe35d2715157c27cdeab83e62f113371f637
yorikav <asonovyury@gmail.com>
2016-06-11 18:58:15 +0500

    Добавили в программу использование метода "Draw" из класса "Точка"

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,32 +14,15 @@ namespace Snake
             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
-            Draw(p1.x, p1.y, p1.sym);
-
+            p1.Draw();
+            
             Point p2 = new Point();
             p2.x = 4;
             p2.y = 5;
             p2.sym = '#';
-            Draw(p2.x, p2.y, p2.sym);
-
-            /*int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);
-
-            int x2 = 3;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);*/
+            p2.Draw();
 
             Console.ReadLine();
-        }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
         }
     }
 }
