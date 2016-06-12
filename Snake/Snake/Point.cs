821bb2750883c42ded51bf5d81d717065b2c4932
yorikav <asonovyury@gmail.com>
2016-06-12 16:08:08 +0500

    Изменен для корректного вывода последней строки

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -22,7 +22,7 @@ namespace Snake
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
+            Console.Write(sym);
         }
     }
 }
