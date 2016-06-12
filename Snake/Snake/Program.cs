b6d8ae005d2115edfe8ce379e430f2e8069d2ea7
yorikav <asonovyury@gmail.com>
2016-06-12 12:33:38 +0500

    Адаптировали основную программу под новый класс "HorizontalLine"

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,7 +16,7 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            HorizontalLine line = new HorizontalLine();
+            HorizontalLine line = new HorizontalLine(3, 9, 12, '+');
             line.Drow();
 
             Console.ReadLine();
