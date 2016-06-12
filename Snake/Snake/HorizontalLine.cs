fe1a43f67a8367d23badb04151b216b36f2d4737
yorikav <asonovyury@gmail.com>
2016-06-12 12:32:55 +0500

    Модернизировали класс "HorizontalLine" для задания координат линии через входные параметры

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -10,15 +10,14 @@ namespace Snake
     {
         List<Point> pList;
 
-        public HorizontalLine()
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
-            Point p1 = new Point(4, 4, '*');
-            Point p2 = new Point(5, 4, '*');
-            Point p3 = new Point(6, 4, '*');
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
         }
 
         public void Drow()
